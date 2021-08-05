6a092f159ea1d420c6e2d36d17971953e10fbd47
virtualpatterns.com <code@virtualpatterns.com>
2021-08-05 13:58:54 -0400

    updates

--- source/index.cjs
+++ source/index.cjs
@@ -1,3 +1,27 @@
-module.exports = function () {
-  return require('@virtualpatterns/mablung-makefile/package.json').babel
+module.exports = function (api, option) {
+
+  // "babel": {
+  //   "overrides": [
+  //     {},
+  //     {
+  //       "exclude": [
+  //         "source/header/**/*",
+  //         "source/sandbox/**/*",
+  //         "source/test/**/*"
+  //       ],
+  //       "plugins": []
+  //     }
+  //   ]
+  // },
+
+  let configuration = require('@virtualpatterns/mablung-makefile/package.json').babel
+
+  let exclude = null
+  exclude = option.header?.exclude || []
+  exclude = Array.isArray(exclude) ? exclude : [ exclude ]
+  
+  configuration.overrides[1].exclude.push(...exclude)
+
+  return configuration
+
 }
--- source/test/index.test.js
+++ source/test/index.test.js
@@ -1,7 +1,133 @@
+import { createRequire as CreateRequire } from 'module'
+import Babel from '@babel/core'
 import Test from 'ava'
 
-import Configuration from '../index.cjs'
+import { Package } from './library/package.js'
 
-Test('index.cjs', (test) => {
-  test.truthy(Configuration())
+const Require = CreateRequire(import.meta.url)
+
+Test('source/index.cjs', async (test) => {
+
+  let codeIn = 'export const OK = true'
+  let option = {
+    'babelrc': false,
+    'filename': 'source/index.cjs',
+    'presets': [
+      Require.resolve('../index.cjs')
+    ]
+  }
+
+  let { code: actualCodeOut } = await Babel.transformAsync(codeIn, option)
+  let expectedCodeOut = '/**\n' +
+                        '* \n' +
+                        '* Package:     @virtualpatterns/babel-preset-mablung-makefile\n' +
+                        '* Description: TBD\n' +
+                        `* Version:     ${Package.version}\n` +
+                        '* License:     GPL-3.0+\n' +
+                        '* Author:      virtualpatterns.com <code@virtualpatterns.com> (http://www.virtualpatterns.com)\n' +
+                        '* Repository:  https://github.com/virtualpatterns/babel-preset-mablung-makefile\n' +
+                        '* Source:      undefined\n' +
+                        '* \n' +
+                        '**/\n' +
+                        '"use strict";\n' +
+                        '\n' +
+                        'Object.defineProperty(exports, "__esModule", {\n' +
+                        '  value: true\n' +
+                        '});\n' +
+                        'exports.OK = void 0;\n' +
+                        '\n' +
+                        'const OK = true;\n' +
+                        'exports.OK = OK;'
+
+  test.is(actualCodeOut, expectedCodeOut)
+
+})
+
+Test('source/index.js', async (test) => {
+
+  let codeIn = 'export const OK = true'
+  let option = {
+    'babelrc': false,
+    'filename': 'source/index.js',
+    'presets': [
+      Require.resolve('../index.cjs')
+    ]
+  }
+
+  let { code: actualCodeOut } = await Babel.transformAsync(codeIn, option)
+  let expectedCodeOut = '/**\n' +
+                        '* \n' +
+                        '* Package:     @virtualpatterns/babel-preset-mablung-makefile\n' +
+                        '* Description: TBD\n' +
+                        `* Version:     ${Package.version}\n` +
+                        '* License:     GPL-3.0+\n' +
+                        '* Author:      virtualpatterns.com <code@virtualpatterns.com> (http://www.virtualpatterns.com)\n' +
+                        '* Repository:  https://github.com/virtualpatterns/babel-preset-mablung-makefile\n' +
+                        '* Source:      undefined\n' +
+                        '* \n' +
+                        '**/\n' +
+                        '\n' +
+                        'export const OK = true;'
+
+  test.is(actualCodeOut, expectedCodeOut)
+
+})
+
+Test('source/index.js { header: { exclude: \'...\' } }', async (test) => {
+
+  let codeIn = 'export const OK = true'
+  let option = {
+    'babelrc': false,
+    'filename': 'source/index.cjs',
+    'presets': [
+      [
+        Require.resolve('../index.cjs'),
+        {
+          'header': {
+            'exclude': 'source/index.cjs'
+          }
+        }
+      ]
+    ]
+  }
+
+  let { code: actualCodeOut } = await Babel.transformAsync(codeIn, option)
+  let expectedCodeOut = '"use strict";\n' +
+                        '\n' +
+                        'Object.defineProperty(exports, "__esModule", {\n' +
+                        '  value: true\n' +
+                        '});\n' +
+                        'exports.OK = void 0;\n' +
+                        'const OK = true;\n' +
+                        'exports.OK = OK;'
+
+  test.is(actualCodeOut, expectedCodeOut)
+
+})
+
+Test('source/index.js { header: { exclude: [ ... ] } }', async (test) => {
+
+  let codeIn = 'export const OK = true'
+  let option = {
+    'babelrc': false,
+    'filename': 'source/index.js',
+    'presets': [
+      [
+        Require.resolve('../index.cjs'),
+        {
+          'header': {
+            'exclude': [
+              'source/index.js'
+            ]
+          }
+        }
+      ]
+    ]
+  }
+
+  let { code: actualCodeOut } = await Babel.transformAsync(codeIn, option)
+  let expectedCodeOut = 'export const OK = true;'
+
+  test.is(actualCodeOut, expectedCodeOut)
+
 })
