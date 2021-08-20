61ca5a466b05e1a8f0a46bcfb7461e5ed83f8d04
virtualpatterns.com <code@virtualpatterns.com>
2021-08-19 22:58:07 -0400

    updates

--- source/test/dependency.test.js
+++ source/test/dependency.test.js
@@ -7,8 +7,10 @@ Test('(default)', async (test) => {
 
   let unused = await Check(Process.cwd(), {
     'parsers': {
-      '**/*.cjs': [ Check.parser.es6, Check.parser.es7.default ],
-      '**/*.js': [ Check.parser.es6, Check.parser.es7.default ]
+      '**/*.cjs': [ Check.parser.es6 ],
+      '**/*.js': [ Check.parser.es6 ]
+      // '**/*.cjs': [ Check.parser.es6, Check.parser.es7.default ],
+      // '**/*.js': [ Check.parser.es6, Check.parser.es7.default ]
     }
   })
 
