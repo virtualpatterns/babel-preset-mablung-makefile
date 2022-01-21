d765a55f65a4a911acc44b7744be7c6a1958c091
virtualpatterns.com <code@virtualpatterns.com>
2022-01-20 19:39:26 -0500

    updates

--- source/index.cjs
+++ source/index.cjs
@@ -14,44 +14,15 @@ module.exports = function (api, option) {
   //           {}
   //         ]
   //       ]
-  //     },
-  //     {
-  //       "exclude": [
-  //       ],
-  //       "plugins": [
-  //         [
-  //           "babel-plugin-add-header-comment",
-  //           {
-  //              "header": [
-  //                "!node get-header.js"
-  //              ]
-  //            }
-  //         ]
-  //       ]
   //     }
   //   ]
   // },
 
-  let path = Path.resolve(`${__dirname}/../babel.config.json`)
+  let path = Path.resolve(__dirname, '../babel.config.json')
   let configuration = FileSystem.readJsonSync(path, { 'encoding': 'utf-8' })
 
   Assert.strictEqual(configuration.overrides[0].presets[0][0], '@babel/preset-env')
   configuration.overrides[0].presets[0][0] = require('@babel/preset-env')
-
-  Assert.strictEqual(configuration.overrides[1].plugins[0][0], 'babel-plugin-add-header-comment')
-  configuration.overrides[1].plugins[0][0] = require('babel-plugin-add-header-comment')
-
-  let exclude = null
-  exclude = option.header?.exclude || []
-  exclude = Is.array(exclude) ? exclude : [ exclude ]
-
-  configuration.overrides[1].exclude = exclude
-
-  let content = null
-  content = option.header?.content || [ '!npx mablung-makefile get-header' ]
-  content = Is.array(content) ? content : [ content ]
-
-  configuration.overrides[1].plugins[0][1].header = content
 
   return configuration
 
